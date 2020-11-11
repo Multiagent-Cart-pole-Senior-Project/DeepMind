2e31cb458efe7c72fbb27bab95eeb28b8acfb111
Ryan Russell <rcrussell@mail.bradley.edu>
2020-11-11 11:46:22 -0600

    Adding comments and starting on Q-learning with linear controller

--- DeepMind_CartPole_LinearController_withQLearning.py
+++ DeepMind_CartPole_LinearController_withQLearning.py
@@ -1,6 +1,7 @@
 from dm_control import suite
 from dm_control import viewer
 import numpy as np
+import matplotlib.pyplot as plt
 
 env = suite.load(domain_name="cartpole", task_name="balance")
 action_spec = env.action_spec()
@@ -9,25 +10,33 @@ action_spec = env.action_spec()
 time_step = env.reset()
 
 # Initialize Matricies
-K = np.array([ 1, 1, 1, 1])
-
 u = np.zeros(10001)
 x_dot = np.zeros(10001)
 theta_dot = np.zeros(10001)
 x = np.zeros(10001)
 theta = np.zeros(10001)
 
-# k = 0
-# kf = 10000
+# Initial Control Gains - From MATLAB Linear Model Simulation
+K = np.array([-12.2595, -2.5696, -0.3670, -0.7391])
+
+# Initialize Q-tables (One for each control gain)
+
+
+k = 0
+kf = 10000
 
 # while k <= kf:
+  # # State Variables
   # x_dot[k] = time_step.observation['velocity'][0]
   # theta_dot[k] = time_step.observation['velocity'][1]
   # x[k] = time_step.observation['position'][0]
   # theta[k] = time_step.observation['position'][1]
   
+  # # Calculate Control Input  
   # x_vec = np.array([[x[k]], [x_dot[k]], [theta[k]], [theta_dot[k]]])
   # u[k] = np.matmul(-K,x_vec)
+  
+  # # Apply Control Input  
   # time_step = env.step(u[k])
   # print("reward = {}, discount = {}, observations = {}.".format(
     # time_step.reward, time_step.discount, time_step.observation))  
@@ -36,9 +45,6 @@ theta = np.zeros(10001)
 
 # Define a linear control policy.
 def linear_control_policy(time_step):
-  # Linear Control Gain
-  K = np.array([-12.2595, -2.5696, -0.3670, -0.7391])
-  
   # State Variables
   x_dot = time_step.observation['velocity'][0]
   theta_dot = time_step.observation['velocity'][1]
@@ -50,8 +56,7 @@ def linear_control_policy(time_step):
   u = np.matmul(-K,x_vec)
   
   # Apply Control Input
-  time_step = env.step(u)
-  print("Angle = {}, Angular Velocity = {}".format(theta, theta_dot))   
+  time_step = env.step(u) 
   return u  
 
 # Launch the viewer application.
