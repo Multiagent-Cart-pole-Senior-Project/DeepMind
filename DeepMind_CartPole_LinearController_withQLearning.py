86e118ff018adaa79da4fdef500e7d6abda20514
Ryan Russell <rcrussell@mail.bradley.edu>
2020-11-16 10:59:17 -0600

    Edits to Q-Learning Control Gains
    The found control gains can now be saved to a pickle for later use. Now, the vectors are also limited such that it prevents an error that sometimes occurs where the range would go to 12 instead of 11.

--- DeepMind_CartPole_LinearController_withQLearning.py
+++ DeepMind_CartPole_LinearController_withQLearning.py
@@ -2,6 +2,9 @@ from dm_control import suite
 from dm_control import viewer
 import numpy as np
 import matplotlib.pyplot as plt
+from tqdm import tqdm
+import pickle
+import time
 
 env = suite.load(domain_name="cartpole", task_name="balance")
 action_spec = env.action_spec()
@@ -9,41 +12,87 @@ action_spec = env.action_spec()
 # Initial Time Step
 time_step = env.reset()
 
-# Initialize Matricies
-u = np.zeros(10001)
-x_dot = np.zeros(10001)
-theta_dot = np.zeros(10001)
-x = np.zeros(10001)
-theta = np.zeros(10001)
-
 # Initial Control Gains - From MATLAB Linear Model Simulation
 K = np.array([-12.2595, -2.5696, -0.3670, -0.7391])
+step = np.array([2,1,0.5,0.1,0.05,0.01])
+indices = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+EPOCHS = 6
 
-# Initialize Q-tables (One for each control gain)
+for epoch in range(EPOCHS):
+    print("EPOCH", epoch)
+    print("K Step Size:", step[epoch])
 
+    # Initialize Q-table
+    q_1 = np.take(np.arange(K[0] - 5*step[epoch], K[0] + 6*step[epoch], step[epoch]), indices).reshape(11,1)
+    q_2 = np.take(np.arange(K[1] - 5*step[epoch], K[1] + 6*step[epoch], step[epoch]), indices).reshape(11,1)
+    q_3 = np.take(np.arange(K[2] - 5*step[epoch], K[2] + 6*step[epoch], step[epoch]), indices).reshape(11,1)
+    q_4 = np.take(np.arange(K[3] - 5*step[epoch], K[3] + 6*step[epoch], step[epoch]), indices).reshape(11,1)
 
-k = 0
-kf = 10000
-
-# while k <= kf:
-  # # State Variables
-  # x_dot[k] = time_step.observation['velocity'][0]
-  # theta_dot[k] = time_step.observation['velocity'][1]
-  # x[k] = time_step.observation['position'][0]
-  # theta[k] = time_step.observation['position'][1]
-  
-  # # Calculate Control Input  
-  # x_vec = np.array([[x[k]], [x_dot[k]], [theta[k]], [theta_dot[k]]])
-  # u[k] = np.matmul(-K,x_vec)
-  
-  # # Apply Control Input  
-  # time_step = env.step(u[k])
-  # print("reward = {}, discount = {}, observations = {}.".format(
-    # time_step.reward, time_step.discount, time_step.observation))  
-  # k = k + 1
+    Q = np.random.uniform(low=0,high=1,size=(11,11,11,11))
 
 
-# Define a linear control policy.
+    # Run System with Q-learning to Optimize Control Gains
+
+    # Parameters
+    Learning_Rate = 0.1
+    Discount = 0.99
+    Episodes = 20_000
+
+    ep_rewards = []
+
+    # Go through Episodes for Training
+    for episode in tqdm(range(Episodes)):
+        done = False
+        episode_reward = 0.0
+        
+        # Reset Environment
+        time_step = env.reset()
+        
+        # Determine Control gains for Episode or random gains
+        K1 = np.random.randint(0,10)
+        K2 = np.random.randint(0,10)
+        K3 = np.random.randint(0,10)
+        K4 = np.random.randint(0,10)
+            
+        # Determine control gain vector
+        K = np.array([q_1[K1], q_2[K2], q_3[K3], q_4[K4]])
+        
+        while not done:                      
+            # State Variables
+            x_dot = time_step.observation['velocity'][0]
+            theta_dot = time_step.observation['velocity'][1]
+            x = time_step.observation['position'][0]
+            theta = np.arccos(time_step.observation['position'][1])
+
+            # Calculate Control Input
+            x_vec = np.array([[theta], [theta_dot], [x], [x_dot]])
+            u = np.matmul(-np.transpose(K),x_vec)
+
+            # Apply Control Input
+            time_step = env.step(u) 
+          
+            if time_step.discount is None:
+                done = True
+
+            if not done:
+                episode_reward += time_step.reward
+        
+        # Save the Episode Total Rewards
+        ep_rewards.append(episode_reward)
+        
+        # Update Q-values for each control gain
+        max_future_q = np.max(Q)
+        current_q = Q[K1][K2][K3][K4]
+        new_q = (1-Learning_Rate) * current_q + Learning_Rate * (episode_reward + Discount * max_future_q)
+        Q[K1][K2][K3][K4] = new_q
+
+    # Redefine K based on optimal control values
+    MAX_Q_TABLE = np.unravel_index(np.argmax(Q), Q.shape)
+    K = np.array([q_1[MAX_Q_TABLE[0]], q_2[MAX_Q_TABLE[1]], q_3[MAX_Q_TABLE[2]], q_4[MAX_Q_TABLE[3]]])
+
+print(K)
+
+# Define a linear control policy. 
 def linear_control_policy(time_step):
   # State Variables
   x_dot = time_step.observation['velocity'][0]
@@ -53,7 +102,7 @@ def linear_control_policy(time_step):
   
   # Calculate Control Input
   x_vec = np.array([[theta], [theta_dot], [x], [x_dot]])
-  u = np.matmul(-K,x_vec)
+  u = np.matmul(-np.transpose(K),x_vec)
   
   # Apply Control Input
   time_step = env.step(u) 
@@ -61,3 +110,7 @@ def linear_control_policy(time_step):
 
 # Launch the viewer application.
 viewer.launch(env, policy=linear_control_policy)
+
+# Save K
+with open(f"K-{int(time.time())}.pickle", "wb") as f:
+    pickle.dump(K, f)
